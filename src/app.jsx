5f7195dd7688322e27f03adf03356fd56aa657ed
JulianCabanillas <julian.cabanillaseng@outlook.es>
2025-05-24 03:04:29 +0200

    [UPD]Add more decorative elements, add pages and more images,

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'preact/hooks'
 import titulo from './assets/titulo.png'
-import flecha from './assets/flecha.png'
+import flechaIzq from './assets/flechaIzq.png'
+import flechaDer from './assets/flechaDer.png'
+import stl from './assets/stl.png'
 import './app.css'
 
 export function App() {
   const [archiveStl, setArchiveStl] = useState(null);
-  const [velocity, setVelocity] = useState(30);
-  const [material, setMaterial] = useState("pla");
-  const [time, setTime] = useState(null)
+  const [velocity, setVelocity] = useState(null);
+  const [material, setMaterial] = useState(null);
+  const [materialRequired, setMaterialRequired] = useState (null);
+  const [time, setTime] = useState(null);
   const [pto, setPto] = useState(null);
 
   return (
@@ -15,39 +18,39 @@ export function App() {
       <div>
         <img src={titulo} class="title" alt="Titulo App" />
       </div>
-      <h1>Sube tu archivo <strong>.stl</strong> para calcular presupuesto!</h1>
+      <h1>Sube tu archivo <img src={stl} alt="" /> para calcular presupuesto!</h1>
       <hr></hr>
       <h2><b>📦 Enviamos a toda españa! </b></h2>
       <h2><blink><mark><code>ENVIO GRATIS</code></mark></blink> a partir de 20€ 🔥🔥🔥</h2>
       <hr></hr>
-      <div>
-        <img src={flecha} class="flecha" alt="flechaIzquierda" />
-        
-        <button class="selectOptions">
+      <div class="options-container">
+        <img src={flechaDer} class="flecha" alt="flechaDerecha" />
+        <button class="botonUp">
           Subir archivo...
         </button>
+        <img src={flechaIzq} class="flecha" alt="flechaIzquierda" />
       </div>
       <div class="options-container">
         <table class="tableOptions">
           <caption>
-            <h2><b><u>Elige tus opciones:</u></b></h2>
+            <h2><b>Elige tus opciones:</b></h2>
           </caption>
           <tbody>
             <tr>
               <th>
-                <p class="pOption">Material: </p>
+                <p class="pOption"><h2>🌳 Material: </h2></p>
               </th>
               <th>
                 <select class= "selectOptions" name="materialChoose" id="materialIdChoose">
-                  <option value="pla">PLA</option>
-                  <option value="abs">ABS</option>
-                  <option value="ptge">PTEG</option>
+                  <option value="pla"><h2>PLA</h2></option>
+                  <option value="abs"><h2>ABS</h2></option>
+                  <option value="ptge"><h2>PTEG</h2></option>
                 </select>
               </th>
             </tr>
             <tr>
               <th>
-                <p class="pOption">Velocidad: </p>
+                <p class="pOption"><h2>🏎️ Velocidad: </h2></p>
               </th>
               <th>
                 <select class= "selectOptions" name="velocityChoose" id="materialIdChoose">
@@ -59,32 +62,60 @@ export function App() {
             </tr>
             <tr>
               <th>
-                <p class="pOption">Color:</p>
+                <p class="pOption"><h2>🔰 Color:</h2></p>
               </th>
               <th>
                 <select class= "selectOptions" name="colorChoose" id="materialIdChoose">
-                  <option value="negro">Negro ⚫</option> 
-                  <option value="blanco">Blanco ⚪</option>         
-                  <option value="verde">Verde 🟢</option>
-                  <option value="rojo">Rojo 🔴</option>
-                  <option value="amarillo">Amarillo 🟡</option>
-                  <option value="naranja">Naranja 🟠</option>
+                  <option value="negro">⚫ Negro </option> 
+                  <option value="blanco">⚪ Blanco </option>         
+                  <option value="verde">🟢 Verde </option>
+                  <option value="rojo">🔴 Rojo </option>
+                  <option value="amarillo">🟡 Amarillo </option>
+                  <option value="naranja">🟠 Naranja </option>
                 </select>
               </th>
             </tr>
           </tbody>
         </table>
-        <div> 
-          <button class="botonPto" >
-            CALCULAR Presupuesto!
-            <progress value="0" max="100"></progress>
-          </button>
-          <p> Presupuesto ------------------
-            <output name='pto'>0.0</output>€
-          </p>
+        <div class="options-container">
+            <table class="tableResults">
+              <caption>
+                <h2><b>Resultados:</b></h2>
+              </caption>
+              <tr>--------------------------------------------------------------------------</tr>
+              <tr>
+                <th>
+                  <p class="pResult"><h2>Tiempo:</h2></p>
+                </th>
+                <th>
+                  <p class="pResult"><h2>- h.{time}</h2></p>
+                </th>
+              </tr>
+              <tr>
+                <th>
+                  <p class="pResult"><h2>Cantidad material:</h2></p>
+                </th>
+                <th>
+                  <p class="pResult"><h2>- m.{materialRequired}</h2></p>
+                </th>
+              </tr>
+              <tr>
+                <th>
+                  <p class="pResult"><h2>Presupuesto:</h2></p>
+                </th>
+                <th>
+                  <h2 class="pResult"> {pto}- €</h2>
+                </th>
+              </tr>
+              <tr>--------------------------------------------------------------------------</tr>
+            </table> 
         </div>
       </div>
-      
+      <button class="botonPto" >
+              <h3>▶️ CALCULAR Presupuesto!</h3>
+              <progress value="10" max="100"></progress>
+            
+          </button>
       <div class="nothing"/>  
       <div>
         <p>Realizada por Julian Cabanillas Blanco.</p>
