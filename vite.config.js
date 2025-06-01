f95d78373048ece3f3d17fa14848f8c48cd30600
JulianCabanillas <julian.cabanillaseng@outlook.es>
2025-06-01 02:34:20 +0200

    [UPD]Upd division for dev and prod (no probe)  -v0.4.8

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [preact()],
+export default defineConfig(({ command, mode }) => {
+  return {
+    base: mode === 'production' ? '/' : '/',
+    plugins: [preact()],
+    server: {
+      host: '0.0.0.0',
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8000',
+          changeOrigin: true,
+        },
+      },
+    },
+    build: {
+      outDir: 'dist',
+    },
+  }
 })
