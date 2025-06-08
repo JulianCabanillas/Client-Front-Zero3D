f1c100505bc065394d6367f5e746035ec5542769
JulianCabanillas <julian.cabanillaseng@outlook.es>
2025-06-09 01:23:29 +0200

    [UPD]Add ORDERS (no probe)  -v0.9.0A

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import { resolve } from 'path'; 
 
 export default defineConfig(({ command, mode }) => {
   return {
     base: mode === 'production' ? '/' : '/',
     plugins: [preact()],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),   // ← añade esto
+      },
+    },
     server: {
       host: '0.0.0.0',
       port: 3000,
